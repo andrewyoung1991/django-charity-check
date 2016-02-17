a0c9c1a23236948a3d2cd9195b5055eae4705a66
Andrew Young <andrewyoung1991@gmail.com>
2016-02-17 11:04:18 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup
 
 setup(
+    name="django-charity-check",
+    version="1.0",
+    author="Andrew Young",
+    author_email="ayoung@thewulf.org",
+    description="A utility plugin for verifying non-profits IRS status.",
     packages=["charity_check"]
     )
